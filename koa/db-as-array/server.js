00bd3a8472795c5f6587898497e985871c4ed380
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2020-03-01 06:50:31 -0300

    fix code style

--- koa/db-as-array/server.js
+++ koa/db-as-array/server.js
@@ -1,27 +1,27 @@
-const koa = require("koa")
-const bodyparser = require("koa-bodyparser")
-const Router = require("koa-router")
-const { v4 } = require("uuid")
+const koa = require("koa");
+const bodyparser = require("koa-bodyparser");
+const Router = require("koa-router");
+const { v4 } = require("uuid");
 
-const app = new koa()
-const router = new Router()
+const app = new koa();
+const router = new Router();
 
-app.use(bodyparser())
+app.use(bodyparser());
 
-let db = []
+let db = [];
 
 const petsController = {
   list: ctx => {
-    ctx.response.body = db
+    ctx.response.body = db;
   },
   show: ctx => {
     const pet = db.filter(f => f.id === ctx.params.id);
-    ctx.response.body = pet
+    ctx.response.body = pet;
   },
   add: ctx => {
     const pet = ctx.request.body;
     db.push({ id: v4(), ...pet });
-    ctx.response.body = `Pet added with name: ${pet.name} and specie ${pet.species}`
+    ctx.response.body = `Pet added with name: ${pet.name} and specie ${pet.species}`;
   },
 }
 
@@ -33,4 +33,4 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(3000, () => console.log('server is running on port 3000...'))
+app.listen(3000, () => console.log("server is running on port 3000..."));
